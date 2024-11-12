c8bada358165095b77c61466796c8582d7a64f5d
bongjj77 <bongjj77@gmail.com>
2024-11-12 11:25:51 +0900

    temp

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { MediasoupGateway } from './mediasoup/mediasoup.gateway';
-import { MediasoupService } from './mediasoup/mediasoup.service';
 import { MediasoupModule } from './mediasoup/mediasoup.module';
 
 @Module({
